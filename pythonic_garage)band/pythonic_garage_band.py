cfba7c28bda4cee7b642afd83c8d38b911ee6971
Katrina Hill <khill365@gmail.com>
2022-04-21 17:30:03 -0700

    added class and objects

---
+++ pythonic_garage)band/pythonic_garage_band.py
@@ -0,0 +1,14 @@
+
+class Musician():
+
+    def __init__(self):
+        pass
+
+    def Guitarist(self):
+        pass
+
+    def Bassist(self):
+        pass
+
+    def Drummer(self):
+        pass
